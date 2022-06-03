a9066e71b865e1bde414f734fbe3677ea0b50a59
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-06-03 12:20:35 +0300

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,4 +2,4 @@ export const Url = "https://ipfs.moralis.io:2053/ipfs";
 
 export const logoM = "https://ipfs.moralis.io:2053/ipfs/QmWEsG4ayh75BMk2H1CowAdALPjsi3fD7CSZ6qxNM1yNnz/image/moralis.png";
 
-export const sampleURI = "https://ipfs.moralis.io:2053/ipfs/QmVHWhgphg7EL3RNd1X1ZsmqtLMzJgzjxLtLdqac6uts22"
+export const sampleURI = "https://ipfs.moralis.io:2053/ipfs/Qmd7DuscoYu3bqBavGxcxvoR1yZDhp8B4sNncyorZphucM"
